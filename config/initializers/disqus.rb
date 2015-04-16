6babd3a6f5f9ab9b409f47b667be5096a1ae17fd
JP <bonnerjp@gmail.com>
2015-04-16 06:38:32 -0400

    ip

---
+++ config/initializers/disqus.rb
@@ -0,0 +1,6 @@
+DisqusRails.setup do |config|
+  config::SHORT_NAME = "your_short_name"
+  config::SECRET_KEY = "2yDw91sVKK8IQJxssyRk8NniBTNI4ZbFom39gSpQZLiuiYpnQggNmaVO2eppktMZ"
+  config::PUBLIC_KEY = "your public_disqus_key" #leave blank if not used
+  config::ACCESS_TOKEN = "your_access_token" #you got it, right? ;-)
+end
