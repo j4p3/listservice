1fcd7639e25fed7339f58a443e238322fd5aacc3
JP <bonnerjp@gmail.com>
2015-04-17 15:54:41 -0400

    working infinite scroll. style for 500

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -5,11 +5,12 @@
 $(document).on('ready page:load', function () {
   window.disqus_rails = new DisqusRails({"short_name":"listservicetest","public_key":"Xitrb3e3FNzRTQ84ruC0YBD35uEVU3prS1EMGM3VDNoVnG6bjbPMKhTo63KTF89H"});
   $(document).trigger('disqus.load');
+  bindInfiniteScroll();
 })
 
-$(function() {
+function bindInfiniteScroll() {
   if ($('#infinite-scrolling').size() > 0) {
-    $(window).on('scroll', function() {
+    $(window).off('scroll').on('scroll', function() {
       var more_posts_url;
       more_posts_url = $('.pagination a[rel=next]').attr('href');
       if (more_posts_url && $(window).scrollTop() > $(document).height() - $(window).height() - 60) {
@@ -19,6 +20,8 @@ $(function() {
           success: function(data) {
             $("#posts").append($(data).find('#posts').html());
             $('#infinite-scrolling').append($(data).find('#infinite-scrolling').html());
+            bindInfiniteScroll();
+            $(document).scrollTop(0);
           }
         });
       }
@@ -27,4 +30,4 @@ $(function() {
       }
     });
   }
-});
+}
