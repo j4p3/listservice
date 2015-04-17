42d710d3943fccf04e8f7f87a786bebee06bd6e3
JP <bonnerjp@gmail.com>
2015-04-17 16:04:04 -0400

    commenting: set disqus keys in env

--- config/initializers/disqus.rb
+++ config/initializers/disqus.rb
@@ -1,6 +1,6 @@
 DisqusRails.setup do |config|
-  config::SHORT_NAME = "listservicetest"
-  config::SECRET_KEY = "qu1j45MVSeSnSS1p8b5lec8VQUtHZIh1mdbntQA90xRW0gatduzZwE4HhTWPdJRU"
-  config::PUBLIC_KEY = "Xitrb3e3FNzRTQ84ruC0YBD35uEVU3prS1EMGM3VDNoVnG6bjbPMKhTo63KTF89H"
-  config::ACCESS_TOKEN = "de9b813a6cd044168ddb920add07e0f1"
+  config::SHORT_NAME = "#{ENV["DISQUS_SHORT_NAME"]}"
+  config::SECRET_KEY = "#{ENV["DISQUS_SECRET_KEY"]}"
+  config::PUBLIC_KEY = "#{ENV["DISQUS_PUBLIC_KEY"]}"
+  config::ACCESS_TOKEN = "#{ENV["DISQUS_ACCESS_TOKEN"]}"
 end
